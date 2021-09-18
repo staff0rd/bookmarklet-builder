b846f0f7b687c4ed22d29d2238d9a2b38ac7acc6
Stafford Williams <stafford.williams@gmail.com>
2021-09-18 18:37:45 +1000

    tabpanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Editor from "@monaco-editor/react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import DarkIcon from "@mui/icons-material/NightsStay";
+import LightIcon from "@mui/icons-material/WbSunny";
+import ToggleButton from "@mui/material/ToggleButton";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
+enum EditorMode {
+  Code = "CODE",
+  Bookmarklet = "BOOKMARKLET",
 }
 
 function App() {
+  const [mode, setMode] = useState(EditorMode.Code);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: EditorMode) =>
+    setMode(newValue);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: isDarkMode ? "dark" : "light",
+        },
+      }),
+    [isDarkMode]
+  );
+
   return (
-    <div>
-      <Editor
-        height="90vh"
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        defaultValue="// some comment"
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Box sx={{ width: "100%", typography: "body1" }}>
+          <TabContext value={mode}>
+            <TabPanel value={EditorMode.Code}>
+              <Button>Format</Button>
+              <ToggleButton
+                value="check"
+                selected={isDarkMode}
+                onChange={() => {
+                  setIsDarkMode(!isDarkMode);
+                }}
+              >
+                {isDarkMode && <DarkIcon fontSize="small" />}
+                {!isDarkMode && <LightIcon fontSize="small" />}
+              </ToggleButton>
+            </TabPanel>
+            <TabPanel value={EditorMode.Bookmarklet}>Item Two</TabPanel>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Code" value={EditorMode.Code} />
+                <Tab label="Bookmarklet" value={EditorMode.Bookmarklet} />
+              </TabList>
+            </Box>
+          </TabContext>
+        </Box>
+        <Editor
+          height="90vh"
+          defaultLanguage="javascript"
+          theme={isDarkMode ? "vs-dark" : ""}
+          defaultValue="console.log('something'); const other = () => 'a function that returns a string'"
+        />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
