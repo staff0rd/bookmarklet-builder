91d30e9607f4250a127ef34bc5e0da315b087694
Stafford Williams <stafford.williams@gmail.com>
2021-09-19 16:42:11 +1000

    scoped css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ScopedCssBaseline } from "@mui/material";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ScopedCssBaseline>
+      <App />
+    </ScopedCssBaseline>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
